42d2aca9cfbf866109a60b777887bf684aab4f9e
Joe Valookaran <joevalookaran@hotmail.com>
2022-12-09 16:09:02 -0500

    finished assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm:NgForm;
+  defaultsubscription="Basic"
+  user = {
+    email: '',
+    subscriptions: '',
+    password: ''
+  }
+  submitted = false;
 
+  onSubmit() {
+    this.user.email = this.signupForm.value.email;
+    this.user.subscriptions = this.signupForm.value.subscriptions;
+    this.user.password = this.signupForm.value.password;
+    this.submitted = true;
+  }
+//   onSubmit(form: NgForm) {
+//     console.log(form)
+//   }
 }
